f2bec781d01ac2a745f679ee49c490144d8974e9
Chris Hampu <chrishampu@outlook.com>
2017-11-17 18:20:12 -0800

    Add rinks page

--- src/main/java/codingonice/ForwardingController.java
+++ src/main/java/codingonice/ForwardingController.java
@@ -8,7 +8,8 @@ public class ForwardingController {
 
     @RequestMapping({
       "/login",
-      "/register"
+      "/register",
+      "/rinks"
     })
     public String forward() {
         return "forward:/";
