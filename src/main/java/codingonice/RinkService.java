54f992590afb46f15bd5c3524a2fab0a59fe9753
Chris Hampu <chrishampu@outlook.com>
2017-11-22 13:16:29 -0800

    Implement getRinks route

--- src/main/java/codingonice/RinkService.java
+++ src/main/java/codingonice/RinkService.java
@@ -1,19 +1,26 @@
 package codingonice;
 
+import codingonice.RinkRepository;
+
 public class RinkService {
 
     private RinkRepository rinkRepository;
-    private RinkService instance;
+    private static RinkService instance;
 
     private RinkService() {
 
+        this.rinkRepository = new RinkRepository();
     }
 
-    public RinkService getInstance() {
+    public static RinkService getInstance() {
         if (instance == null) {
             instance = new RinkService();
         }
         return instance;
+    }
+
+    public RinkRepository getRepository() {
+        return this.rinkRepository;
     }
         
     public Rink save() {
