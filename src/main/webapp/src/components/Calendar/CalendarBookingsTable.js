9a1b13143e8dd9d1a873bf0a0ed32eb7d0677b0c
Unknown <chrishampu@outlook.com>
2017-11-22 09:57:54 -0800

    Calculate position for event in the time table

--- src/main/webapp/src/components/Calendar/CalendarBookingsTable.js
+++ src/main/webapp/src/components/Calendar/CalendarBookingsTable.js
@@ -1,20 +1,9 @@
 import React from "react";
 import { withStyles } from "material-ui/styles";
+import { CircularProgress } from "material-ui/Progress";
 
 const currentDate = new Date();
-const currentMonth = currentDate.getMonth();
-
-/*
-const formatTime = date => {
-  let minutes =
-    date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
-  let hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
-
-  return `${hours > 12 ? hours - 12 : hours}:${minutes}${
-    hours >= 12 ? "pm" : "am"
-  }`;
-};
-*/
+const curMonth = currentDate.getMonth();
 
 const formatTime = date =>
   `${date.getHours() > 12 ? date.getHours() - 12 : date.getHours()}${
@@ -23,36 +12,114 @@ const formatTime = date =>
 
 const times = day =>
   [9, 10, 11, 12, ...Array.from(Array(5)).map((_v, i) => i + 13)].map(hour => {
-    return new Date(2017, currentMonth, day, hour, 0, 0, 0);
+    return new Date(2017, curMonth, day, hour, 0, 0, 0);
   });
 
 const styles = theme => ({
   container: {
-    marginTop: '0.5rem',
+    marginTop: "0.5rem",
     flex: 1,
-    display: 'flex'
+    display: "flex"
   },
   times: {
-    display: 'flex',
-    flexDirection: 'column',
-    textAlign: 'right',
-    borderRightWidth: '1px',
-    borderRightStyle: 'solid',
+    display: "flex",
+    flexDirection: "column",
+    textAlign: "right",
+    borderRightWidth: "1px",
+    borderRightStyle: "solid",
     borderRightColor: theme.palette.grey[500],
-    paddingRight: '0.5rem'
+    paddingRight: "0.5rem",
+    marginRight: "0.5rem"
   },
   time: {
     ...theme.typography.caption,
-    flex: 1
+    flex: 1,
+    display: "flex",
+    alignItems: "center"
+  },
+  bubbles: {
+    position: "relative",
+    flex: 1,
+    width: "100%"
+  },
+  bubble: {
+    position: "absolute",
+    textAlign: "left"
   }
 });
 
-export default withStyles(styles)(
-  ({ classes: c, bookings, contract, timeTable, day }) => (
-    <div className={c.container}>
-      <div className={c.times}>
-        {times(day).map(time => <div className={c.time}>{formatTime(time)}</div>)}
+// start is the starting hour for an event in 24 hr time
+const getOffset = (ref, day, times, start) => {
+  const comparison = new Date(2017, curMonth, day, start, 0, 0, 0).getTime();
+  const end = new Date(2017, curMonth, day, start + 1, 0, 0, 0).getTime();
+  const first = times[0].getTime();
+  const last = times[times.length - 1].getTime();
+
+  const pixelDiff = ref.clientHeight - 0;
+  const timeDiff = last - first;
+
+  const ratio = timeDiff / pixelDiff;
+
+  const timeOffset = comparison - first;
+  const endOffset = end - first;
+  const pixelOffset = timeOffset / ratio;
+  const heightOffset = endOffset / ratio;
+
+  return { top: pixelOffset, height: heightOffset - pixelOffset };
+};
+
+const startTimes = [10, 13, 15];
+
+class CalendarBookingsTable extends React.PureComponent {
+  containerRef = null;
+
+  updateRef(ref) {
+    // The timeout accounts for the time it takes for the table element
+    // to transition and reach its full height before the offsets are calculated
+    setTimeout(() => {
+      this.containerRef = ref;
+      this.setState(() => ({ a: "b" })); // This is a dirty hack to be removed
+    }, 500);
+  }
+
+  render() {
+    const containerRef = this.containerRef;
+    const { classes: c, bookings, day } = this.props;
+
+    return (
+      <div className={c.container} ref={ref => this.updateRef(ref)}>
+        <div className={c.times}>
+          {times(day).map(time => (
+            <div key={time.getTime()} className={c.time}>
+              {formatTime(time)}
+            </div>
+          ))}
+        </div>
+        <div className={c.bubbles}>
+          {!containerRef ? (
+            <CircularProgress />
+          ) : (
+            bookings.slice(0, 3).map(({ name }, i) => (
+              <div
+                key={i}
+                className={c.bubble}
+                style={{
+                  top:
+                    getOffset(containerRef, day, times(day), startTimes[i % 3])
+                      .top + "px",
+                  height:
+                    getOffset(containerRef, day, times(day), startTimes[i % 3])
+                      .height + "px"
+                }}
+              >
+                {name}
+              </div>
+            ))
+          )}
+        </div>
       </div>
-    </div>
-  )
-);
+    );
+  }
+}
+
+export default withStyles(styles)(CalendarBookingsTable);
