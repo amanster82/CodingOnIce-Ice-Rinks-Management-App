7e94a62b0310bf6c6e7f593f952cd36d879f6401
Chris Hampu <chrishampu@outlook.com>
2017-11-22 15:48:30 -0800

    Implement entity classes, rink service, rink retrieval

--- src/main/java/codingonice/Booking.java
+++ src/main/java/codingonice/Booking.java
@@ -2,11 +2,29 @@ package codingonice;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "bookings")
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private int id;
+
+    @ManyToOne
     private Rink rink;
+
+    @ManyToOne
     private Account account;
+
     private Date startTime;
+
     private int length;
 
     private Booking() {
--- src/main/java/codingonice/Rink.java
+++ src/main/java/codingonice/Rink.java
@@ -2,15 +2,49 @@ package codingonice;
 
 import java.util.ArrayList;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "rinks")
 public class Rink {
 
-    private int id;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
     private boolean underMaintenance;
+    
     private String name;
+
     private ArrayList<Booking> bookings;
+
+    @OneToMany
+    public ArrayList<Booking> getBookings() {
+        return bookings;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean getUnderMaintenance() {
+        return underMaintenance;
+    }
+
+    public void setBookings(ArrayList<Booking> bookings) {
+        this.bookings = bookings;
+    }
 
     private Rink() {
         
     }
-
 }
