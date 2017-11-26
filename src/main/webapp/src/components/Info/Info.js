93632bead0cfebcf91a3028a3d7385fc75e561d5
Ryan Djeric <ryandjeric@gmail.com>
2017-11-26 14:03:16 -0800

    Added basic layout and information to info page

--- src/main/webapp/src/components/Info/Info.js
+++ src/main/webapp/src/components/Info/Info.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
-import Paper from 'material-ui/Paper';
-import Typography from 'material-ui/Typography';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "material-ui/styles";
+import Paper from "material-ui/Paper";
+import Typography from "material-ui/Typography";
+import rink1Image from "./images/map.png";
+
 
 const styles = theme => ({
+  backgroundStyle: theme.mixins.gutters({
+    paddingTop: 1,
+  }),
   root: theme.mixins.gutters({
     paddingTop: 16,
     paddingBottom: 16,
+    maxWidth: 400,
+    marginLeft: 50,
+    marginTop: theme.spacing.unit * 3,
+  }),
+  mapStyle: theme.mixins.gutters({
+    paddingTop: 16,
+    paddingBottom: 16,
+    maxWidth: 625,
+    marginLeft: 50,
     marginTop: theme.spacing.unit * 3,
   }),
 });
@@ -15,14 +29,39 @@ const styles = theme => ({
 function InfoSheet(props) {
   const { classes } = props;
   return (
-    <div>
-      <Paper className={classes.root} elevation={4}>
-        <Typography type="headline" component="h3">
-          Arena Complex
+    <div className={classes.backgroundStyle} style={{backgroundImage: 'url(http://www.trip-points.com/media/reviews/photos/original/4f/12/a8/32-minsk-arena-complex-29-1443951652.jpg)'}}>
+      <Paper className={classes.root}>
+        <Typography type="headline" color="secondary">
+          Commonwealth Arena Complex
         </Typography>
-        <Typography type="body1" component="p">
-          Info
+      </Paper>
+      <Paper className={classes.root}>
+        <Typography type="headline" color="secondary">
+          Arena Information
         </Typography>
+        <Typography type="body1">
+        Commonwealth has 9 indoor ice arenas that can be used by groups for a variety of activites.
+        </Typography>
+      </Paper>
+      <Paper className={classes.root}>
+        <Typography type="headline" color="secondary">
+          Contact
+        </Typography>
+        <Typography type="body1">
+          <b>Address:</b> 1925 Patricia Bay Hwy, Victoria, B.C.
+        </Typography>
+        <Typography type="body1">
+          <b>Phone Number:</b> (555) 555-8296
+        </Typography>
+        <Typography type="body1">
+          <b>Email:</b> commonwealtharenacomplex@shaw.ca
+        </Typography>
+      </Paper>
+      <Paper className={classes.mapStyle}>
+        <Typography type="headline"  color="secondary">
+          Map
+        </Typography>
+        <img src ="https://i.imgur.com/mw9OtWs.png" />
       </Paper>
     </div>
   );
