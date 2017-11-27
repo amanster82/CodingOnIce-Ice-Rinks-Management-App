7606ebff27031fa034a458a35559fc91d451a2d7
Chris Hampu <chrishampu@outlook.com>
2017-11-27 15:10:47 -0800

    Get account state into user icon component

--- src/main/webapp/src/components/Navigation/UserIcon.js
+++ src/main/webapp/src/components/Navigation/UserIcon.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { withStyles } from "material-ui/styles";
+import { connect } from "react-redux";
+import { compose } from "recompose";
 
-export default () => (
-  <div>logged in</div>
+const styles = theme => ({
+
+});
+
+const mapStateToProps = store => ({
+  ...store.accounts.current
+})
+
+const enhance = compose(
+  withStyles(styles),
+  connect(mapStateToProps)
 );
+
+// add menu links for: profile, logout
+
+export default enhance(({classes: c, name}) => (
+  <div>{name}</div>
+));
