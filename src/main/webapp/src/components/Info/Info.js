52e48d65c48ba917160fc36de909fc81e9eebded
Chris Hampu <chrishampu@outlook.com>
2017-11-27 19:33:47 -0800

    Improve info page background

--- src/main/webapp/src/components/Info/Info.js
+++ src/main/webapp/src/components/Info/Info.js
@@ -14,10 +14,18 @@ import Map from "material-ui-icons/Map";
 
 
 const styles = theme => ({
-  backgroundStyle: ({
-    paddingTop: 1,
-    height: "100%",
-  }),
+  backgroundStyle: {
+    backgroundSize: 'cover',
+    height: '100%',
+    overflow: 'hidden'
+  },
+  overlay: {
+    ...theme.mixins.gutters({
+      paddingTop: 1,
+    }),
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    height: '100%'
+  },
   root: theme.mixins.gutters({
     paddingTop: theme.spacing.unit * 2,
     paddingBottom: theme.spacing.unit * 2,
@@ -38,64 +46,42 @@ const styles = theme => ({
 function InfoSheet(props) {
   const { classes } = props;
   return (
-    <div className={classes.backgroundStyle} style={{backgroundImage: 'url(' + rinkBackground + ')'}}>
-      <Paper className={classes.root}>
-        <Typography type="headline" color="primary">
-          Commonwealth Arena Complex
-        </Typography>
-      </Paper>
-      <Paper className={classes.root}>
-        <Typography type="headline" color="primary">
-          Arena Information
-        </Typography>
-        <Typography type="body1">
-        Commonwealth has 9 indoor ice arenas that can be used by groups for a variety of activites.
-        </Typography>
-      </Paper>
-      <Paper className={classes.root}>
-        <Typography type="headline" color="primary">
-          Contact
-        </Typography>
-        <List>
-          <ListItem>
-            <ListItemIcon>
-              <Avatar>
-                <Today/>
-              </Avatar>
-            </ListItemIcon>
-            <Typography type="body1">
-              1925 Patricia Bay Hwy, Victoria, B.C.
-            </Typography>
-          </ListItem>
-          <ListItem>
-            <ListItemIcon>
-              <Avatar>
-                <Call/>
-              </Avatar>
-            </ListItemIcon>
-            <Typography type="body1">
-              (555) 555-8296
-            </Typography>
-          </ListItem>
-          <ListItem>
-            <ListItemIcon>
-              <Avatar>
-                <MailOutline/>
-              </Avatar>
-            </ListItemIcon>
-            <Typography type="body1">
-              commonwealtharenacomplex@shaw.ca
-            </Typography>
-          </ListItem>
-        </List>
-      </Paper>
-      <Paper className={classes.mapStyle}>
-        <Typography type="headline"  color="primary">
-        <Map/>
-          Map
-        </Typography>
-        <img src = {rinkMap} alt=""/>
-      </Paper>
+    <div className={classes.backgroundStyle} style={{backgroundImage: 'url(http://www.trip-points.com/media/reviews/photos/original/4f/12/a8/32-minsk-arena-complex-29-1443951652.jpg)'}}>
+      <div className={classes.overlay}>
+        <Paper className={classes.root}>
+          <Typography type="headline" color="secondary">
+            Commonwealth Arena Complex
+          </Typography>
+        </Paper>
+        <Paper className={classes.root}>
+          <Typography type="headline" color="secondary">
+            Arena Information
+          </Typography>
+          <Typography type="body1">
+          Commonwealth has 9 indoor ice arenas that can be used by groups for a variety of activites.
+          </Typography>
+        </Paper>
+        <Paper className={classes.root}>
+          <Typography type="headline" color="secondary">
+            Contact
+          </Typography>
+          <Typography type="body1">
+            <b>Address:</b> 1925 Patricia Bay Hwy, Victoria, B.C.
+          </Typography>
+          <Typography type="body1">
+            <b>Phone Number:</b> (555) 555-8296
+          </Typography>
+          <Typography type="body1">
+            <b>Email:</b> commonwealtharenacomplex@shaw.ca
+          </Typography>
+        </Paper>
+        <Paper className={classes.mapStyle}>
+          <Typography type="headline"  color="secondary">
+            Map
+          </Typography>
+          <img src ="https://i.imgur.com/mw9OtWs.png" />
+        </Paper>
+      </div>
     </div>
   );
 }
