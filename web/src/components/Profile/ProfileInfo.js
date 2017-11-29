d7e0ee15cfa922e40655e3d63859f4fc097b3910
Ryan Djeric <ryandjeric@gmail.com>
2017-11-29 10:16:33 -0800

    Style changes to Profile Info page

--- web/src/components/Profile/ProfileInfo.js
+++ web/src/components/Profile/ProfileInfo.js
@@ -1,15 +1,10 @@
 import React from "react";
 import { withStyles } from "material-ui/styles";
-import PermIdentity from "material-ui-icons/PermIdentity";
 import Avatar from "material-ui/Avatar";
-import Button from "material-ui/Button";
 import List, { ListItem, ListItemIcon, ListItemText } from "material-ui/List";
-import IconButton from "material-ui/IconButton";
-import CreditCard from "material-ui-icons/CreditCard";
-import ModeEdit from "material-ui-icons/ModeEdit";
 import Face from "material-ui-icons/Face";
 import MailOutline from "material-ui-icons/MailOutline";
-import Today from "material-ui-icons/Today";
+import Done from "material-ui-icons/Done";
 import { connect } from "react-redux";
 import { compose } from "recompose";
 
@@ -51,7 +46,7 @@ export default enhance(({ classes: c, account }) => (
       <ListItem>
         <ListItemIcon>
           <Avatar>
-            <MailOutline />
+            <Done />
           </Avatar>
         </ListItemIcon>
         <ListItemText
